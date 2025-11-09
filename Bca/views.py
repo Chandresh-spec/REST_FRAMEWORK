f88c85ca674a1e3cfb5d8caff68c1203d71e0f00
Chandresh-spec <chandumoger819@gmail.com>
2025-11-09 13:22:14 +0530

    learned serialization in restframework

--- Bca/views.py
+++ Bca/views.py
@@ -1,29 +1,46 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+from .models import Student
+from .serializers import StudentSerializers
 # Create your views here.
 
 
 
 @api_view(['GET','POST','PUT'])
 def hello_api(request):
-    data={
-           "name":"chandres",
-           "subject":['kannada','english','ds','python'],
-           "class":"3rd year"
-        }
+    if request.method== "GET":
+        st=Student.objects.all()
+        serializer=StudentSerializers(st,many=True)
+        return Response(serializer.data)
     
-    if request.method=='POST':
-        print("YOU HIT POST METHPD")
-        return Response(data)
+    elif request.method=='POST':
+        serializer=StudentSerializers(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response("data saved sucessfully")
+        
+        return Response(serializer.errors)
     
-    elif request.method=='GET':
-        print("YOU HIT GET METHPD")
-        return Response(data)
-       
-    
+
     elif request.method=='PUT':
-        print("YOU HIT PUT METHPD")
-        return Response(data)
+        id=request.GET.get('id')
+
+        st=Student.objects.get(id=id)
+
+        serializer=StudentSerializers(st,data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response("data saved sucesdfully")
+        return Response(serializer.errors)
+    
+
+
+
+
 
 
