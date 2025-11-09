a8820c70c0555a75b199d6807b80605f560aa25d
Chandresh-spec <chandumoger819@gmail.com>
2025-11-09 16:18:16 +0530

    patch

--- Bca/views.py
+++ Bca/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
@@ -8,23 +8,23 @@ from .serializers import StudentSerializers
 
 
 
-@api_view(['GET','POST','PUT'])
-def hello_api(request):
-    if request.method== "GET":
+@api_view(['GET','POST','PUT','PATCH','DELETE'])
+def  hello_api(request):
+    if  request.method=="GET":
         st=Student.objects.all()
+
         serializer=StudentSerializers(st,many=True)
+
         return Response(serializer.data)
-    
+
     elif request.method=='POST':
         serializer=StudentSerializers(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return Response("data saved sucessfully")
-        
+            return Response("data saved sucesfully")
         return Response(serializer.errors)
     
-
     elif request.method=='PUT':
         id=request.GET.get('id')
 
@@ -34,12 +34,36 @@ def hello_api(request):
 
         if serializer.is_valid():
             serializer.save()
-
-            return Response("data saved sucesdfully")
+            return Response("data updated sucessfully")
+        
         return Response(serializer.errors)
     
 
 
+    elif request.method=='PATCH':
+        id=request.GET.get('id')
+
+        st=Student.objects.get(id=id)
+
+        serializer=StudentSerializers(st,data=request.data,partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response("data patched sucesfully")
+        
+        return Response(serializer.errors)
+    
+
+        
+
+    elif request.method=="DELETE":
+        id=request.GET.get("id")
+
+        st=get_object_or_404(Student,id=id)
+        st.delete()
+
+        return Response("deleted sucesfully")
 
 
 
